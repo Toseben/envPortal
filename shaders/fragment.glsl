8a7caa989c3de15aad89298dcd4a29544a7d6683
Arttu <arekoskela@gmail.com>
2017-02-05 10:33:29 +0200

    Debug Android bug

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -30,12 +30,14 @@ void main() {
     // Deform Amount
     float deform = 1.0 - click;
     //deform = 0.0;
+    
     // Scale Up
     float xOffset = elementPos.x;
     float yOffset = elementPos.y * deform;
     vec2 scaledUV = vec2(vUv.x + xOffset, vUv.y + yOffset) * 2.0 - 1.0;
     scaledUV.x *= 2.0;
     vec2 ratio = vec2(scaledUV.x * 0.5, scaledUV.y);
+    //ratio = clamp(ratio, 0.0, 1.0);
     
     // Anim
     float hoverMult = ease(min(1.0, hoverTime)) + 1.0;
@@ -45,14 +47,16 @@ void main() {
     anim = mix(anim, 1.0, click);
     //anim = 1.0;
     float circle = sqrt((anim) - deform * dot(scaledUV, scaledUV));
-    circle = min(max(0.0, circle), 1.0);
+    circle = clamp(circle, 0.0, 1.0);
     vec2 sampleUV = ratio / circle;
+    
     // Scale Down
     sampleUV = (sampleUV + 1.0) * 0.5;
+    sampleUV = clamp(sampleUV, 0.0, 1.0);
     
     // Final Texture
     float alpha = range(0.0, 0.1, circle);
     gl_FragColor = vec4(vec3(texture2D(textureEnv, sampleUV, 0.0)), alpha);
-    //gl_FragColor = vec4(vec3(sampleUV, 0.0), 1.0);
+    //gl_FragColor = vec4(vec3(sampleUV.x >= 0.0), 1.0);
     
 }
